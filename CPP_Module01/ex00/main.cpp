be081c1a2c9b62aac51014e5fac9f2f36eae420a
alfux <alexis.t.fuchs@gmail.com>
2022-10-24 03:58:42 +0200

    modified:	CPP_Module01/ex00:	segfault protection in case new throws exception

--- CPP_Module01/ex00/main.cpp
+++ CPP_Module01/ex00/main.cpp
@@ -6,29 +6,47 @@
 /*   By: alfux <alexis.t.fuchs@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/21 20:30:21 by alfux             #+#    #+#             */
-/*   Updated: 2022/10/24 02:52:55 by alfux            ###   ########.fr       */
+/*   Updated: 2022/10/24 03:51:07 by alfux            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "Zombie.hpp"
+
+int	delcrowd(Zombie **crowd)
+{
+	int	i;
+
+	i = 0;
+	while (*(crowd + i))
+		delete *(crowd + i++);
+	delete[] crowd;
+	return (0);
+}
 
 int	main(int ac, char **av)
 {
 	std::string	name;
 	Zombie		**crowd;
+	int			i;
 
-	crowd = new Zombie *[ac];
-	*(crowd + --ac) = (Zombie *)0;
-	while (--ac >= 0)
+	try
 	{
-		*(crowd + ac) = new Zombie(*(av + ac + 1));
-		name = *(av + ac + 1);
-		name += "'s soul";
-		(*(crowd + ac))->announce();
+		crowd = new Zombie *[ac];
+	}
+	catch (std::bad_alloc &bad)
+	{
+		std::cerr << "error: " << bad.what() << std::endl;
+		return (1);
+	}
+	*(crowd + ac - 1) = (Zombie *)0;
+	for (i = 0; (i < ac - 1); i++)
+	{
+		*(crowd + i) = newZombie(*(av + i + 1));
+		if (!*(crowd + i))
+			return (1 + delcrowd(crowd));
+		name = name.assign(*(av + i + 1)) + "'s soul";
+		(*(crowd + i))->announce();
 		randomChump(name);
 	}
-	ac = 0;
-	while (*(crowd + ac))
-		delete *(crowd + ac++);
-	delete[] crowd;
+	delcrowd(crowd);
 	return (0);
 }
