ab6a79e990bd5e175b2b82d39e93f3c4f407a420
alfux <alexis.t.fuchs@gmail.com>
2022-11-05 14:33:40 +0100

    finished:	CPP_Module05/ex01

--- CPP_Module05/ex01/main.cpp
+++ CPP_Module05/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: alfux <alexis.t.fuchs@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/05 14:16:25 by alfux             #+#    #+#             */
-/*   Updated: 2022/11/05 14:20:18 by alfux            ###   ########.fr       */
+/*   Updated: 2022/11/05 14:32:39 by alfux            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "Form.hpp"
@@ -15,9 +15,15 @@ int	main(void)
 {
 	Bureaucrat	pdg("Alexis Fuchs", 1);
 	Form		form;
+	try
+		{Form	test(1, 15);}
+	catch (std::exception &ex)
+		{std::cout << ex.what() << std::endl;}
+	Form		test(15, 1);
 
 	pdg.signForm(form);
 	pdg -= 1;
 	pdg.signForm(form);
+	pdg.signForm(test);
 	return (0);
 }
